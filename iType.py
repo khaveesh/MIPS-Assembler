20923c7792711345c4624d5ecadb450352a53045
k-udupa2000 <karthik.r.udupa@gmail.com>
2019-11-23 01:22:31 +0530

    itype

--- iType.py
+++ iType.py
@@ -3,24 +3,37 @@ from writeToFile import writeToFile
 from getRegisterCode import getRegisterCode
 def iType2Args(reg1, reg2):
     
+    index = reg2.find('(')
+    offset = reg2[0:index]
+    offset = int(offset)
+    binaddr += "{0:04b}".format(int(address[i], 16))
+    index2 = reg2.find(')')
+    
+    secondReg = reg2[index + 1, index2]
+
     regCode1 = getRegisterCode(reg1)
+    
+    
     printColor(regCode1, "red")
     writeToFile(re)
-    regCode2 = getRegisterCode(reg2)
+    
+    regCode2 = getRegisterCode(secondReg)
     printColor(regCode2, "red")
+    writeToFile(regCode2)
 
+    printColor(offset)
+    writeToFile
     # Print 16 bit Immediate value
 
-def iType3Args(reg1, reg2, reg3):
+def iType3Args(reg1, reg2, value):
     regCode1 = getRegisterCode(reg1)
     printColor(regCode1, "")
-    regCode
+    writeToFile(regCode1)
     regCode2 = getRegisterCode(reg2)
     printColor(regCode2, "")
     writeToFile(regCode2)
     regCode3 = getRegisterCode(reg3)
     printColor(regCode3, "")
     writeToFile(regCode3)
-def opCodes():
 
 
