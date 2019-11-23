4944093eadb8ef0b5546b0e1220d17d1595568a4
k-udupa2000 <karthik.r.udupa@gmail.com>
2019-11-23 11:58:51 +0530

    Added codes to getType

--- getType.py
+++ getType.py
@@ -5,19 +5,25 @@ from writeToFile import writeToFile
 
 
 def getType(inst):
-    rType = ["ADD", "SUB", "MULT"]
-    iType = ["ADDI", "LW", "SW", "SUBI"]
+    rType = ['AND', 'SUBU', 'MFLO', 'MFHI', 'SLTU', 'ADDU', 'XOR', 'SUB', 'ADD', 'NOR', 'SRLV', 'SRAV',
+    'DIVU', 'MULT', 'SRA', 'SLT', 'SLLV', 'SRL', 'SLL', 'MTLO', 'MTHI', 'DIV', 'OR', 'MULTU']
+    iType = ["ADDI", "LW", "SW", "SUBI", "SLTI", "BLEZ"]
     opcode = {
+        # R - TYPE
+        # ALL THE R TYPE HAVE 00000000 OPCODE.
+
+        # I - TYPE
         "ADDI": "001000",
+        "SLTI": "001010",
         "LW": "100011",
         "SW": "101011",
         "BEQ": "000100",
-        "J": "000010",
-        "SLT": "101010",
         "BGTZ": "000111",
         "BNE": "000101",
         "BLEZ": "000110",
-        "SLTI": "001010",
+
+        # J - TYPE
+        "J": "000010",
     }
 
     if inst.upper() in rType:
