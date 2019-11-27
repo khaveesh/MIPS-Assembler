4f1ef8f058194e912531040b83c20a66d4fa70eb
k-udupa2000 <karthik.r.udupa@gmail.com>
2019-11-27 12:22:52 +0530

    Added codes

--- getType.py
+++ getType.py
@@ -43,6 +43,10 @@ def getType(inst):
         "BEQ",
         "BGTZ",
         "BLT",
+        "LI",
+        "ORI",
+        "ANDI",
+        "XORI",
     ]
 
     opcode = {
@@ -59,6 +63,12 @@ def getType(inst):
         "BLEZ": "000110",
         "BLT": "100011",
         "BLTZ": "000001",
+        "LI": "001001",
+        "ORI": "001101",
+        "ANDI": "001100",
+        "XORI": "001110",
+        "SUBI": "001000",
+        "CLO": "",
         # J - TYPE
         "J": "000010",
     }
--- iType.py
+++ iType.py
@@ -4,33 +4,51 @@ from getRegisterCode import getRegisterCode
 
 
 def iType2Args(inst, reg1, reg2):
-    l = ["SW", "LW"]
-    if inst.upper() not in l:
-        raise Exception(" Wrong instruction format")
+    if inst.upper() == "LI":
+        regCode2 = getRegisterCode(reg1)
+        printColor(regCode2, "red")
+        writeToFile(regCode2)
 
-    index = reg2.index("(")
-    offset = reg2[0:index]
-    offset = int(offset)
-    if offset % 4:
-        raise Exception(" Boundary is not word aligned")
-    binaddr = ""
-    binaddr += bin(offset).replace("0b", "")
-    binaddr = binaddr.zfill(16)
-    index2 = reg2.index(")")
-    secondReg = reg2[(index + 1) : index2]
+        binaddr = ""
+        if int(reg2) >= 0:
+            binaddr += bin(int(reg2)).replace("0b", "")
+            binaddr = binaddr.zfill(21)
+        else:
+            num = 65535 + int(reg2)
+            num = num + 1
+            num = str(num)
+            binaddr += bin(int(num)).replace("0b", "")
+            binaddr = binaddr.zfill(16)
+        printColor(binaddr + "\n", "green")
+        writeToFile(binaddr + "\n")
+    else:
+        l = ["SW", "LW"]
+        if inst.upper() not in l:
+            raise Exception(" Wrong instruction format")
 
-    regCode2 = getRegisterCode(secondReg)
-    printColor(regCode2, "red")
-    writeToFile(regCode2)
-    regCode1 = getRegisterCode(reg1)
-    printColor(regCode1, "blue")
-    writeToFile(regCode1)
-    printColor(binaddr + "\n", "green")
-    writeToFile(binaddr + "\n")
+        index = reg2.index("(")
+        offset = reg2[0:index]
+        offset = int(offset)
+        if offset % 4:
+            raise Exception(" Boundary is not word aligned")
+        binaddr = ""
+        binaddr += bin(offset).replace("0b", "")
+        binaddr = binaddr.zfill(16)
+        index2 = reg2.index(")")
+        secondReg = reg2[(index + 1) : index2]
+
+        regCode2 = getRegisterCode(secondReg)
+        printColor(regCode2, "red")
+        writeToFile(regCode2)
+        regCode1 = getRegisterCode(reg1)
+        printColor(regCode1, "blue")
+        writeToFile(regCode1)
+        printColor(binaddr + "\n", "green")
+        writeToFile(binaddr + "\n")
 
 
 def iType3Args(inst, reg1, reg2, dest):
-    set1 = ["ADDI", "SUBI", "DIVI", "SLTI"]
+    set1 = ["ADDI", "SUBI", "DIVI", "SLTI", "ORI", "ANDI", "XORI"]
     set2 = ["BEQ", "BNE", "BLT", "BGT"]
     binaddr = ""
 
@@ -40,7 +58,6 @@ def iType3Args(inst, reg1, reg2, dest):
             binaddr += bin(int(dest)).replace("0b", "")
             binaddr = binaddr.zfill(16)
         else:
-            #print(dest)
             num = 65535 + int(dest)
             num = num + 1
             #print(num)
@@ -52,6 +69,8 @@ def iType3Args(inst, reg1, reg2, dest):
     elif inst.upper() in set2:
         for i in range(6, len(dest)):
             binaddr += "{0:04b}".format(int(dest[i], 16))
+        binaddr = binaddr[0:-2]
+        binaddr = "00" + binaddr
 
     regCode2 = getRegisterCode(reg2)
     printColor(regCode2, "red")
